bf4455040e7fb9f70608d1e45233274649ac239d
ricky <39970880+torickyto@users.noreply.github.com>
2024-08-06 00:00:11 -0600

    added user session for dashboard

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './Dashboard.css';
 import CalendarView from './CalendarView';
 import AuthGuard from './components/auth/AuthGuard';
+import { useSession } from '@supabase/auth-helpers-react';
 
 const Dashboard = ({ globalSearchTerm }) => {
+  const session = useSession();
+  const userFirstName = session?.user?.user_metadata?.first_name || 'User';
   return (
     <AuthGuard>
       <div className="dashboard-content">
-        <CalendarView searchTerm={globalSearchTerm} />
+        <CalendarView searchTerm={globalSearchTerm} userFirstName={userFirstName}/>
       </div>
     </AuthGuard>
   );
