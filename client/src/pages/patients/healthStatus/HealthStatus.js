e480f69032d0152e3b7d224ce4bd24e7f7cf0fb0
Alex <Lam.Alexander@hotmail.com>
2024-09-19 21:05:10 -0600

    Linked buttons to pages

--- client/src/pages/patients/healthStatus/HealthStatus.js
+++ client/src/pages/patients/healthStatus/HealthStatus.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './HealthStatus.css';
+import { Link } from 'react-router-dom';
 
 const HealthStatus = () => {
     return (
+        <div className='patient-main'>
+         <header className="patient-header">
+                <div className="patient-tabs">
+                    <Link to="/patient/clinical" className="tab-button">Clinical</Link>
+                    <Link to="/patient/soc" className="tab-button">S.O.C.</Link>
+                    <Link to="/Financial" className="tab-button">Financial</Link>
+                    <Link to="/patient/summaries" className="tab-button">Summaries</Link>
+                    <Link to="/healthStatus" className="tab-button">Health Status</Link>
+                    <Link to="/patient/medication" className="tab-button">Medication</Link>
+                    <Link to="/newPatient" className="tab-button">New Patient</Link>
+                </div>
+            </header>
         <div className="health-status-container">
+            
             <div className="section-box">
                 <section className="canine-info">
                     <header className="section-header">CANINE INFORMATION</header>
@@ -57,6 +71,8 @@ const HealthStatus = () => {
                     </div>
                 </section>
             </div>
+        </div>
+
         </div>
     );
 };
