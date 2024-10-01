54ef5a5d0311f78cd09e8912af9de6a79c7f2685
Squilto <edwardouston@shaw.ca>
2024-10-01 04:53:56 -0600

    Added basic use effects, and dummy data for testing, as well as updated structure but maybe will need reversion

--- client/src/pages/patients/soc/SOC.js
+++ client/src/pages/patients/soc/SOC.js
@@ -1,17 +1,52 @@
 import React from "react";
-import './SOC.css'
+import './SOC.css';
 import PatientSidebar from "../../../components/patientSideBar/PatientSidebar";
 
 const SOC = () => {
+    return (
+        <div className="SOC_container">
+            <PatientSidebar />
 
-    return(
-        <div className="SOC_container"> 
-           <PatientSidebar/>
-          <div className="standard-of-care">
-          </div>
-        {/* <Comments /> */}
-      </div>
-  );
+            {/* Standard of Care Section */}
+            <div className="standard-of-care">
+                <h2>+ Standard Of Care</h2>
+
+                <div className="soc-table">
+                    {/* Row 1 */}
+                    <div className="soc-row">
+                        <div className="soc-item">Annual Health Check</div>
+                        <div className="soc-importance required">Required</div>
+                        <div className="soc-fulfilled">25-07-2024</div>
+                        <div className="soc-next-due">01-04-2025</div>
+                    </div>
+                    
+                    {/* Row 2 */}
+                    <div className="soc-row">
+                        <div className="soc-item">DA2PP</div>
+                        <div className="soc-importance core">Core</div>
+                        <div className="soc-fulfilled">15-04-2025</div>
+                        <div className="soc-next-due">TBD</div>
+                    </div>
+                    
+                    {/* Additional rows can be added similarly */}
+                </div>
+            </div>
+
+            {/* Comments Section */}
+            <div className="comments-section">
+                <h2>+ Comments (1)</h2>
+                <div className="comment">
+                    <p>Annual Health Check next due date moved to 01-04-2025</p>
+                    <div className="comment-meta">
+                        <span>Public</span>
+                        <span>28-05-2024</span>
+                        <span>Edward Ling</span>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
 };
 
 export default SOC;
