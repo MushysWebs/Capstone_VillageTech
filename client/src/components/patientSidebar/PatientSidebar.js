67e5785fb6e45a3d9877da2a4be8eb3c3e91a4cf
Squilto <edwardouston@shaw.ca>
2024-09-30 21:06:18 -0600

    added better styling to the side component

--- client/src/components/patientSidebar/PatientSidebar.js
+++ client/src/components/patientSidebar/PatientSidebar.js
@@ -5,44 +5,62 @@ import "./PatientSidebar.css"
 const PatientSidebar = () => {
 
 
+
+
     
     return (
         <div className="PatientSidebar">
-            <div className="patientPhoto">
-             {/* <img src="https://fuitfkjuphldahkcsogq.supabase.co/storage/v1/object/public/contacts/profile_pictures/0.38716950783300974.jpg" alt="Taylor Swift" class="contact-header-avatar"> */}
+            <div className="patientImg">
+             <img src="https://fuitfkjuphldahkcsogq.supabase.co/storage/v1/object/public/contacts/profile_pictures/0.38716950783300974.jpg" alt="Taylor Swift" class="contact-header-avatar"></img>
              {/* This is Rickys code for his contacts page anime taylor swift something, ill dissect later */}
             </div> 
-            <div className="patientName">
+
+            <div className="patientInfo">
                 
                 <h2 className="patientName">Ponzu</h2>
-                <dl>
-                <p>Patient ID: </p>
+                <dl class='patient-body'>
+                    <p>Patient ID: </p>
                     <p>Date of Birth: </p>
-                    <p></p>
                     <p>Weight:  kg</p>
                 </dl>
-                {/* <Link> </Link> */}
                 </div>
          
-            <div className="demeanor">
-
-                <h3>demeanor</h3>
-                <p><span>Evil</span></p><img></img>
-
-            </div>
-            <div className="appointments">
-                <p>Upcoming Appointment</p>
-
-                <p>Last Appointment</p>
-
-                <p>Recourring Appointments</p>
-                
+            <div className="patientDemeanor">
+                <h3 class="demeanor-header">Demeanor</h3>
+                <div class='demeanor-container'>
+                    <p>Evil</p>
+                    <i class="fa-regular fa-pen-to-square"></i>
+                </div>
 
             </div>
-            <div className="SOCEvent">
-                <p>SOC Event</p><p>Next Due</p>
+            <div className="patient-owner">
+                <h3>Owner</h3>
+                <p class='owner-name'>Grady</p>
+                <p>403123456789</p>
+
+            </div>
+
+            <div className="patientApp">
+                    <h3>Appointments</h3>
                 
 
+                <p>Next Appointment: </p>
+                <link></link>
+                <p>Last Appointment: </p>
+                <link></link>
+                <p>Recourring Appointments: </p>
+                <link></link>
+
+            </div>
+            <div className="patientSOC">
+                <div className="event-section">
+                    <h3 class='soc-event'>SOC Event</h3>
+                    <h3 class='next-due'>Next Due</h3>
+                </div>
+                <dl class='SOC-body'>
+                    <link></link>
+                    <link></link>
+                </dl>
             </div>
         </div>
     );
--- client/src/pages/patients/soc/SOC.js
+++ client/src/pages/patients/soc/SOC.js
@@ -6,30 +6,9 @@ const SOC = () => {
 
     return(
         <div className="SOC_container"> 
-            <PatientSidebar/>
-        <div className="standard-of-care">
-          <h2>Standard Of Care</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Treatment/Vaccine</th>
-                <th>Importance</th>
-                <th>Fulfilled At</th>
-                <th>Next Due</th>
-              </tr>
-            </thead>
-            {/* <tbody>
-              {treatments.map(treatment => (
-                <tr key={treatment.id}>
-                  <td>{treatment.name}</td>
-                  <td>{treatment.importance}</td>
-                  <td>{treatment.fulfilledAt || 'TBD'}</td>
-                  <td>{treatment.nextDue || 'TBD'}</td>
-                </tr>
-              ))}
-            </tbody> */}
-          </table>
-        </div>
+           <PatientSidebar/>
+          <div className="standard-of-care">
+          </div>
         {/* <Comments /> */}
       </div>
   );
