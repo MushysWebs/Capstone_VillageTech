6e8467bbba26a4375084259d02fa1b9833d4a6ac
MushysWebs <grady_spurrill@hotmail.com>
2024-10-10 16:36:08 -0600

    Repushing working routing

--- client/src/pages/patients/soc/SOC.js
+++ client/src/pages/patients/soc/SOC.js
@@ -3,7 +3,7 @@ import { supabase } from "../../../components/routes/supabaseClient";
 import { usePatient } from "../../../context/PatientContext";
 import PatientSidebar from "../../../components/patientSidebar/PatientSidebar";
 import "./SOC.css";
-import PatientTabs from "../../../components/patientSidebar/PatientTabs";
+import PatientTabs from "../../../components/patientsidebar/PatientTabs";
 
 const SOC = () => {
   const { selectedPatient } = usePatient();
