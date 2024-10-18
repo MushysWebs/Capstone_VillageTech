0bdd97014a355673a40d375090400ccf40997049
Squilto <edwardouston@shaw.ca>
2024-10-18 15:44:14 -0600

    Of waverly place zoey 101

---
+++ client/src/components/eodwModal/wizModal.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+
+
+const wizModal = () => {
+
+    return(
+        <div className="theWiz">
+            <div className="theWizHeader">
+                <h2>End of Day Wizard Summary</h2>
+                <div className="clock">time O'clock</div>
+            </div>
+            <div className="theWizMain">
+                <div className="theWizSummary">
+                    <div className="theWiz1">
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                    </div>
+
+                    <div className="theWiz2">
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                    </div>
+                    
+                    <div className="theWiz3">
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+                        <div className="summary_container"><h3>1</h3><p>poo</p></div>
+
+                    </div>
+                </div>
+
+                <div className="theWizOrd">
+                    <div className="graph">
+
+                    </div>
+
+                    <div className="comments">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default wizModal;
---
+++ client/src/pages/reporting/endOfDayWizard/EODW.js
@@ -0,0 +1,15 @@
+import React from "react";
+import wizModal from "../../../components/eodwModal/wizModal";
+
+const EOWD = () => {
+
+
+    return (
+        <div className="EODW_main">
+            <wizModal/>
+            <button></button>
+        </div>
+    );
+};
+
+export default EOWD;
