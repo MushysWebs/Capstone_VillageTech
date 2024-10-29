f55d356a5c3916344f7663a3ced92aa7044ee03b
Alex <Lam.Alexander@hotmail.com>
2024-10-29 16:53:07 -0600

    Basic add function for Medication page

---
+++ client/src/components/addVitalModal/AddVitalModal.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+// import "./AddVitalModal.css";
+
+const AddVitalModal = ({ isOpen, onClose, onAddVital }) => {
+  const [date, setDate] = useState("");
+  const [weight, setWeight] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [heartRate, setHeartRate] = useState("");
+
+  const handleAddVital = () => {
+    const newVital = {
+      date,
+      weight,
+      temperature,
+      heart_rate: heartRate,
+    };
+
+    onAddVital(newVital);
+    onClose();
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setDate("");
+    setWeight("");
+    setTemperature("");
+    setHeartRate("");
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>Add Vital</h2>
+        <form>
+          <label>
+            Date:
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Weight:
+            <input
+              type="text"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </label>
+          <label>
+            Temperature:
+            <input
+              type="text"
+              value={temperature}
+              onChange={(e) => setTemperature(e.target.value)}
+            />
+          </label>
+          <label>
+            Heart Rate:
+            <input
+              type="text"
+              value={heartRate}
+              onChange={(e) => setHeartRate(e.target.value)}
+            />
+          </label>
+          <div className="modal-actions">
+            <button type="button" onClick={handleAddVital}>
+              Add
+            </button>
+            <button type="button" onClick={onClose}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddVitalModal;
